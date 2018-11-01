5e1a2a04dc0b671f152742952049dd7f16cb75fa
MISISTIK <misistik@gmail.com>
2018-11-02 00:24:37 +0200

    Some new methods to MediaPlayer

--- src/main/java/ua/homework/DauchenSoldatenUnterOfficieren/Main.java
+++ src/main/java/ua/homework/DauchenSoldatenUnterOfficieren/Main.java
@@ -1,5 +1,6 @@
 package ua.homework.DauchenSoldatenUnterOfficieren;
 
+import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -9,22 +10,40 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    volatile static MediaPlayer player;
     public static void main(String[] args) {
         try{
             final JFXPanel fxPanel = new JFXPanel();
             String uriString = new File("droeloe-in-time.mp3").toURI().toString();
             Media hit = new Media(uriString);
-            MediaPlayer player = new MediaPlayer(hit);
+            player = new MediaPlayer(hit);
 
-            player.setStartTime(new Duration(35000));
-            player.setStopTime(new Duration(50000));
+            new Thread(() -> {
+                while(true) {
+                    System.out.println(String.format("%.2f",player.getCurrentTime().toSeconds()) + " sec");
+                    try {
+                        Thread.sleep(350);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            player.setStartTime(new Duration(58000));
+            player.setStopTime(new Duration(60000));
+            player.setOnEndOfMedia(() -> {player.seek(Duration.ZERO); player.play();});
             System.out.println("Start time = " + player.getStartTime().toString());
             System.out.println(player.getStatus().toString());
             player.play();
             System.out.println(player.getStatus().toString());
-            TimeUnit.SECONDS.sleep(5);
-            player.stop();
+            TimeUnit.SECONDS.sleep(6);
+            player.pause();
+            TimeUnit.MILLISECONDS.sleep(10);
             System.out.println(player.getStatus().toString());
+            player.play();
+            TimeUnit.MILLISECONDS.sleep(10);
+            System.out.println(player.getStatus().toString());
+            TimeUnit.SECONDS.sleep(6);
             System.exit(0);
         }
         catch(Exception ex)
